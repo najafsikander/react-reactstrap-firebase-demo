3b19f364d693eecbf76d3048ead46c1ceb784de0
trotdrive <trotdrive@gmail.com>
2021-04-19 11:04:38 +0400

    minor fix

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -8,14 +8,6 @@ const firebaseConfig = {
     messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID }`,
     appId: `${process.env.REACT_APP_APP_ID }`,
     measurementId: `${process.env.REACT_APP_MEASUREMENT_ID }`
-    
-    // apiKey: "AIzaSyBidMMFRPvx0MHGrnmIgg9zmDyWzNE59AE",
-    // authDomain: "fir-react-demo-5796e.firebaseapp.com",
-    // projectId: "fir-react-demo-5796e",
-    // storageBucket: "fir-react-demo-5796e.appspot.com",
-    // messagingSenderId: "26766225899",
-    // appId: "1:26766225899:web:2e18a43929bc3b3e38e9ae",
-    // measurementId: "G-BVWNXKD8CD"
   };
 
   firebase.initializeApp(firebaseConfig);
